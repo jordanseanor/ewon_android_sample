ee5057bf13af5fcd79e1799e377421fbc9df7879
jose-hms <jose@hms.se>
2017-07-12 12:44:43 -0400

    Initial project commit

---
+++ M2WebSample/app/src/main/java/hms/ewon/sample/m2websample/Login.java
@@ -0,0 +1,13 @@
+package hms.ewon.sample.m2websample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
