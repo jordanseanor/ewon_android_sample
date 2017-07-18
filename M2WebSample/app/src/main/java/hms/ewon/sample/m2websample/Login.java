535270282b79062beb708dc9cec740c3c9f7eb1e
jose-hms <jose@hms.se>
2017-07-18 14:37:46 -0400

    Login AsyncTask
    Created Login task to manage login to account.

--- M2WebSample/app/src/main/java/hms/ewon/sample/m2websample/Login.java
+++ M2WebSample/app/src/main/java/hms/ewon/sample/m2websample/Login.java
@@ -1,13 +1,20 @@
 package hms.ewon.sample.m2websample;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.pnikosis.materialishprogress.ProgressWheel;
 
 import org.w3c.dom.Text;
 
@@ -53,15 +60,7 @@ public class Login extends AppCompatActivity {
             String account = txtAccount.getText().toString();
             String username = txtUsername.getText().toString();
             String password = txtPassword.getText().toString();
-
-            Request request = new Request();
-            SessionResult result = request.login(account, username, password, id);
-            if(result.getSuccess()){
-                writeToPreferences(id, account, username, password);
-                return true;
-            }else{
-                return false;
-            }
+            new login(account, username, password, id).execute();
         }
         return false;
     }
@@ -93,5 +92,55 @@ public class Login extends AppCompatActivity {
                 .putString(getString(R.string.username_pref), username)
                 .putString(getString(R.string.password_pref), password)
                 .apply();
+    }
+
+    private class login extends AsyncTask<Void, Void, SessionResult>{
+        private String account, username, password, developerId;
+        private ProgressDialog loadingDialog;
+
+        public login(String account, String username, String password, String developerId){
+            this.account = account;
+            this.username = username;
+            this.password = password;
+            this.developerId = developerId;
+            loadingDialog = new ProgressDialog(Login.this);
+        }
+
+        @Override
+        protected void onPreExecute(){
+            loadingDialog.setMessage("Logging in...");
+            loadingDialog.setCancelable(false);
+            loadingDialog.setIndeterminate(true);
+            loadingDialog.show();
+        }
+
+        @Override
+        protected SessionResult doInBackground(Void... voids) {
+            Request request = new Request();
+            return request.login(account, username, password, developerId);
+        }
+
+        @Override
+        protected void onPostExecute(SessionResult result){
+            if(loadingDialog.isShowing()){
+                loadingDialog.dismiss();
+            }
+            
+            if(result.getSuccess()){
+                writeToPreferences(developerId, account, username, password);
+                startActivity(new Intent(Login.this, Dashboard.class));
+            }else{
+                AlertDialog alertDialog = new AlertDialog.Builder(Login.this).create();
+                alertDialog.setTitle("Failure");
+                alertDialog.setMessage("Unable to log into account. Please confirm your entries.");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        }
     }
 }
